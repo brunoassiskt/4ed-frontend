ca7b4e01a90ef21cc271aa594e8d88667e107d37
Bruno Assis <brunoassiskt@gmail.com>
2020-03-02 17:27:36 -0300

    trabalho feito em casa 02-03

--- exercicio_final/main.js
+++ exercicio_final/main.js
@@ -6,8 +6,8 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
-            items:2
+        400:{
+            items:1
         },
         1000:{
             items:3
